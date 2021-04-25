0dd6e90fe4834737b2fe48081fb26d453a243a90
Kathi <74504874+kathi208@users.noreply.github.com>
2021-04-25 04:25:10 +0200

    fixes for mobile version & switched icons in card reader (#24)
    * fixes for mobile version
    * switched icons in card reader
    
    close #15

--- functions.js
+++ functions.js
@@ -17,11 +17,11 @@ const collapse = (id) => {
 
   if (content.style.display.length === 0 || content.style.display === 'none') {
     content.style.display = 'inherit';
-    icon.classList.remove('fa-angle-up');
-    icon.classList.add('fa-angle-down');
-  } else {
-    content.style.display = 'none'
     icon.classList.remove('fa-angle-down');
     icon.classList.add('fa-angle-up');
+  } else {
+    content.style.display = 'none'
+    icon.classList.remove('fa-angle-up');
+    icon.classList.add('fa-angle-down');
   }
 }
